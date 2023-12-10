d9a700a4b8b6af110f64ed2edd66b44b28a14de0
jack <jack.nguyen@weareday.one>
2023-12-10 23:42:55 +0700

    fix: bottom nav

--- app/src/components/BottomNavs.js
+++ app/src/components/BottomNavs.js
@@ -4,7 +4,7 @@ import { Box, Typography, alpha } from '@mui/material';
 const navs = [
   { text: 'Games', path: '/games', icon: '/icons/game.png' },
   { text: 'Chat', path: '/chat', icon: '/icons/message.png' },
-  { text: 'Home', path: '/', icon: '/icons/home.png' },
+  { text: 'Home', path: '/home', icon: '/icons/home.png' },
   {
     text: 'Notifications',
     path: '/notifications',
@@ -16,7 +16,7 @@ const navs = [
 const BottomNavs = () => {
   const navigate = useNavigate();
   const { pathname } = useLocation();
-  const isActive = (path) => path === pathname;
+  const isActive = (path) => path === pathname || pathname.includes(path);
 
   return (
     <Box display="flex" justifyContent="space-between" p={1} bgcolor="#fa5f60">
@@ -33,7 +33,11 @@ const BottomNavs = () => {
           borderRadius={2}
           bgcolor={isActive(nav.path) ? alpha('#fff', 0.1) : 'transparent'}
           sx={{ cursor: 'pointer' }}
-          onClick={() => navigate(nav.path)}
+          onClick={() => {
+            if (!isActive(nav.path)) {
+              navigate(nav.path);
+            }
+          }}
         >
           <img src={nav.icon} alt="icon" width={24} />
           <Typography
