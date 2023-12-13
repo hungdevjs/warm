78328a45bbc6939e6d1a737d5273ac6057e56f57
jack <jack.nguyen@weareday.one>
2023-12-13 21:44:06 +0700

    feat: update logic create user

--- app/functions/triggers/auth.js
+++ app/functions/triggers/auth.js
@@ -3,14 +3,18 @@ const admin = require('firebase-admin');
 const { faker } = require('@faker-js/faker');
 
 const onAuthUserCreated = functions.auth.user().onCreate(async (user) => {
-  await admin.firestore().collection('users').doc(user.uid).set({
-    email: user.email,
-    username: faker.internet.userName(),
-    gender: null,
-    avatarURL: faker.image.avatar(),
-    coupleId: null,
-    createdAt: admin.firestore.FieldValue.serverTimestamp(),
-  });
+  await admin
+    .firestore()
+    .collection('users')
+    .doc(user.uid)
+    .set({
+      email: user.email,
+      username: user.email.split('@')[0],
+      gender: null,
+      avatarURL: faker.image.avatar(),
+      coupleId: null,
+      createdAt: admin.firestore.FieldValue.serverTimestamp(),
+    });
 });
 
 module.exports = { onAuthUserCreated };
