afa5bcd35931abb46aa801e310281409ac521d21
jack <jack.nguyen@weareday.one>
2023-12-21 20:26:25 +0700

    feat: hide bottom navs when open keyboard

--- app/src/components/Layout.js
+++ app/src/components/Layout.js
@@ -1,14 +1,17 @@
 import { Box } from '@mui/material';
+import useDetectKeyboardOpen from 'use-detect-keyboard-open';
 
 import BottomNavs from './BottomNavs';
 
 const Layout = ({ children }) => {
+  const isKeyboardOpen = useDetectKeyboardOpen();
+
   return (
     <Box height="100vh" display="flex" flexDirection="column">
       <Box flex={1} overflow="auto" bgcolor="#f8f8f8">
         {children}
       </Box>
-      <BottomNavs />
+      {!isKeyboardOpen && <BottomNavs />}
     </Box>
   );
 };
